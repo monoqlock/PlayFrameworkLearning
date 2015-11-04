e3759b0e8d6c3c424fbd1abe1936926b47464f7b
森川 かおり <morikawa_kaori@cyberagent.co.jp>
2015-11-04 20:02:11 +0900

    add action/http router section

--- app/controllers/SampleController.scala
+++ app/controllers/SampleController.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import play.api._
+import play.api.libs.iteratee.Enumerator
 import play.api.mvc._
 
 /**
@@ -12,7 +13,31 @@ object SampleController extends Controller {
     Ok(views.html.index("Sample Controller#sample1"))
   }
 
+//  def sample1 = Action {
+//    SimpleResult(
+//      header = ResponseHeader(200, Map(CONTENT_TYPE -> "text/html")),
+//      body = Enumerator(views.html.index("Sample Controller#sample1"))
+//    )
+//  }
+
   def sample2 = Action {
     Ok(views.html.index("Sample Contoroller#sample2"))
   }
+
+  def redirect1 = Action {
+    Redirect("/sample1")
+  }
+
+  def redirect2 = Action {
+    Redirect("http://www.atmarkit.co.jp/")
+  }
+
+  def sample3(id: Long) = Action {
+    Ok(views.html.index("id:" + id))
+  }
+
+  def sample4(path: String) = Action {
+    Ok(views.html.index(path))
+  }
+
 }
